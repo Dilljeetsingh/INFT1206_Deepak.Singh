d66898cccdcee3c4ab58b1e3270a25cb09f5911d
Dilljeetsingh <135757591+Dilljeetsingh@users.noreply.github.com>
2023-11-24 12:41:52 -0500

    close the loop and add image details

--- Assignment 4/part2/gallery-start/main.js
+++ Assignment 4/part2/gallery-start/main.js
@@ -12,14 +12,16 @@ var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 function thumbnailLoop() {
-    var i;
-    for (i = 0; i < 5; i++) {
+var i;
+for (i = 0; i < 5; i++) {
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+var newImage = document.createElement('img');
+newImage.setAttribute('src', "images/pic" + (i+1) + ".jpg");
+
 thumbBar.appendChild(newImage);
 }
 }
+thumbnailLoop();
 
 /* Wiring up the Darken/Lighten button */
+newImage.setAttribute('alt', xxx);
