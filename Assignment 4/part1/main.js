9092bb022e60241b2c3b0d5ee9aa9fc72e9857b8
Dilljeetsingh <135757591+Dilljeetsingh@users.noreply.github.com>
2023-11-24 12:00:53 -0500

    add function

--- Assignment 4/part1/main.js
+++ Assignment 4/part1/main.js
@@ -8,3 +8,8 @@
 	var customName = document.getElementById('customname');
 	var randomize = document.querySelector('.randomize');
 	var story = document.querySelector('.story');
+
+	function randomValueFromArray(array) {
+		return array[Math.floor(Math.random() * array.length)];
+	  }
+	  
