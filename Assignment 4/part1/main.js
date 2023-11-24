a2091b425f1598657debee7fa6c886da0b14903b
Dilljeetsingh <135757591+Dilljeetsingh@users.noreply.github.com>
2023-11-24 12:00:13 -0500

    change the js file name and add  JavaScript to interact with elements in an HTML

---
+++ Assignment 4/part1/main.js
@@ -0,0 +1,10 @@
+/**
+	Name: Deepak Singh
+	File: main.js
+	Date: 24 Nov 2023
+	creating a fun app that generates random silly stories. 
+    */
+
+	var customName = document.getElementById('customname');
+	var randomize = document.querySelector('.randomize');
+	var story = document.querySelector('.story');
--- Assignment 4/part1/script.js
+++
@@ -1,6 +0,0 @@
-/**
-	Name: Deepak Singh
-	File: script.html
-	Date: 24 Nov 2023
-	creating a fun app that generates random silly stories. 
-    */
